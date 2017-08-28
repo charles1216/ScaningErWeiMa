49fbf1a9cc9ec1ac9ab068807d00091699b2f3e3
charles <wushang@charlesdeMacBook-Pro.local>
2017-08-28 14:09:59 +0800

    Initial Commit

---
+++ 扫描二维码/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  扫描二维码
+//
+//  Created by charles on 2017/8/28.
+//  Copyright © 2017年 charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 扫描二维码/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  扫描二维码
+//
+//  Created by charles on 2017/8/28.
+//  Copyright © 2017年 charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
